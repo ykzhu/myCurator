0e7aab2d69026a6bf2bff3f4022470b9203ec0c4
ykzhu <ykzhu@mail.bnu.edu.cn>
2016-06-01 23:39:49 +0800

    test2

--- curator-examples/src/main/java/leader/LeaderSelectorExample.java
+++ curator-examples/src/main/java/leader/LeaderSelectorExample.java
@@ -60,7 +60,7 @@ public class LeaderSelectorExample
                 example.start();
             }
 
-            System.out.println("5.Press enter/return to quit\n");
+            System.out.println("2.Press enter/return to quit\n");
             new BufferedReader(new InputStreamReader(System.in)).readLine();
         }
         finally
