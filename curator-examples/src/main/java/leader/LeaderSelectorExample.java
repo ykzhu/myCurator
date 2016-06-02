261ab5a236f61ef55bcbb3db751599dda0105c3a
ykzhu <ykzhu@mail.bnu.edu.cn>
2016-06-02 10:56:51 +0800

    ceshi

--- curator-examples/src/main/java/leader/LeaderSelectorExample.java
+++ curator-examples/src/main/java/leader/LeaderSelectorExample.java
@@ -58,7 +58,7 @@ public class LeaderSelectorExample
                 example.start();
             }
 
-            System.out.println("2.Press enter/return to quit\n");
+            System.out.println("6.Press enter/return to quit\n");
             new BufferedReader(new InputStreamReader(System.in)).readLine();
         }
         finally
